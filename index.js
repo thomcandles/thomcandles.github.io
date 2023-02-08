bdef4c9b4d98e1691cde281eb7fd7e224f4e2327
thomcandles <124708231+thomcandles@users.noreply.github.com>
2023-02-08 10:09:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,96 @@
-$(document).ready(function() {
-  $.getJSON('https://docs.google.com/spreadsheets/d/16qvshfM6akxUyM-96zJIOmFmpzpy9-WrFtk8JWaU6IE/edit?usp=sharing', function(data) {
-    var sheetData = data.feed.entry;
+const Table = (props) => {
+  return (
+    <table className="table">
+      <thead> 
+        <TableHeader headers={props.headers} />
+      </thead>
+      <tbody>
+        <TableRows rows={props.data} values={props.values} />
+      </tbody>
+    </table>
+  )
+}
 
-    var htmlString = "<table><tr><th>Name</th><th>Data</th></tr>";
+const TableHeader = (props) => {
+  return props.headers.map((e, i) => {
+    return <th>{e}</th>
+  })
+}
 
-    for (var i = 0; i < sheetData.length; i++) {
-      htmlString += "<tr><td>" + data.feed.entry[i]['gsx$name']['$t'] + "</td><td>" + data.feed.entry[i]['gsx$data']['$t'] + "</td></tr>";
-    }
-    htmlString += "</table>"
+const TableRows = (props) => {
+  return props.rows.map((e, i) => { 
+    return (
+      <tr key={i}>  
+        <TableCells row={e} values={props.values} />
+      </tr>
+    )
+  })
+}
 
-    $('#googlesheet').html(htmlString);
-  });
-});
+const TableCells = (props) => {
+  return props.values.map((v, i) => {
+    return (
+      <td>
+        { i === 0 ? 
+          <a href={props.row['url']} target="_blank">{ props.row[v] }</a> 
+          : props.row[v] }
+      </td>
+    )
+  })
+}
+
+class App extends React.Component {
+   constructor() {
+     super();
+     this.state = {
+      items: []
+     }
+   }
+  
+  componentDidMount() {
+    this.fetchGoogleSheet ();
+  }
+  
+  fetchGoogleSheet = () => {
+    // api key restricted to domain/directory
+    const apiKey = 'AIzaSyBFpZregfOoRFucibyPYyp-Vu9RkiR8NQc';
+    const cellRange = '!A2:Z3000'
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/1KxEK0jt7wcaIn4A22_zas5Za8I-l-429dKaZgtICeVk/values/data${cellRange}?key=${apiKey}`;
+    fetch(url)
+    .then(resp => resp.json())
+    .then((data) => {
+      const results = [];
+      data.values.map((val, i) => {
+        let obj = {
+           title: val[0],
+           issuingOffice: val[1],
+           category: val[2],
+           url: val[3]
+        }
+        results.push(obj);
+      });
+      this.setState({
+        items: [...this.state.items, ...results]
+      });
+    })
+    .catch(error => {
+      console.log('There was an error fetching your request.')
+    });
+  }
+ 
+  render() {
+    return (
+      <div>
+        <a className="view-gs" href="https://docs.google.com/spreadsheets/d/1KxEK0jt7wcaIn4A22_zas5Za8I-l-429dKaZgtICeVk/edit?usp=sharing" target="_blank">View Google Sheet</a>
+        <Table 
+          headers={['Title', 'Issuing Office', 'Category']} 
+          data={this.state.items}
+          values={['title', 'issuingOffice', 'category']}
+         />
+      </div>
+    )
+  }
+}
+      
+ReactDOM.render(<App/>, document.getElementById('root'));
+
